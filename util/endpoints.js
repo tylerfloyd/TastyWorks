90af79fbf5c3d7784f83a9e966d91f2a88cee177
sprgn <sprgn@filatov.us>
2019-04-25 09:19:28 -0400

    Added order execute and cancel operations

--- util/endpoints.js
+++ util/endpoints.js
@@ -7,5 +7,8 @@ module.exports = {
     balances: (account_id) => `${baseURL}/accounts/${account_id}/balances`,
     positions: (account_id) => `${baseURL}/accounts/${account_id}/positions`,
     liveOrders: (account_id) => `${baseURL}/accounts/${account_id}/orders/live`,
+    executeOrder: (account_id) => `${baseURL}/accounts/${account_id}/orders`,
+    cancelOrder: (account_id, order_id) => `${baseURL}/accounts/${account_id}/orders/${order_id}`,
     history: (account_id) => `${baseURL}/accounts/${account_id}/transactions`
+    // TODO: '{url}/dry-run'
 }
