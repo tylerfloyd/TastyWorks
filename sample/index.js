182f6c23f28311c305b2ea7423c76d0074dd338c
Tyler <tylerbfloyd@gmail.com>
2019-12-08 14:58:33 -0700

    updated example

--- sample/index.js
+++ sample/index.js
@@ -2,9 +2,11 @@
 
 const TastyWorks = require('../lib/index');
 const credentials = {
-    username: process.env.TASTY_USERNAME,
-    password: process.env.TASTY_PASSWORD
+    username: "rotaryguy", //process.env.TASTY_USERNAME,
+    password: "ZqW@PVGamo15!RU1S7K#" //process.env.TASTY_PASSWORD
 };
+
+let TASTY_ACCOUNT_ID;
 
 // Set the username and password
 TastyWorks.setUser(credentials);
@@ -18,29 +20,35 @@ TastyWorks.authorization()
         return true;
     })
     .then(() => TastyWorks.accounts())
-    .then(accounts => TastyWorks.setUser({
-        accounts
-    }))
+    .catch(err => console.log(err.status))
+    .then(accounts => {
+        TASTY_ACCOUNT_ID = accounts[0]['account-number'];
+        TastyWorks.setUser({accounts});
+    })
     .then(() => {
         console.log('======= USER OBJECT =======');
         console.log(TastyWorks.getUser());
     })
-    .then(() => TastyWorks.balances(process.env.TASTY_ACCOUNT_ID))
+    .then(() => TastyWorks.balances(TASTY_ACCOUNT_ID))
+    .catch(err => console.log(err.status))
     .then(balances => {
         console.log('======= ACCOUNT BALANCES =======');
         console.log(balances)
     })
-    .then(() => TastyWorks.positions(process.env.TASTY_ACCOUNT_ID))
+    .then(() => TastyWorks.positions(TASTY_ACCOUNT_ID))
+    .catch(err => console.log(err.status))    
     .then(positions => {
         console.log('======= ACCOUNT POSITIONS =======');
         console.log(positions)
     })
-    .then(() => TastyWorks.liveOrders(process.env.TASTY_ACCOUNT_ID))
+    .then(() => TastyWorks.liveOrders(TASTY_ACCOUNT_ID))
+    .catch(err => console.log(err.status))
     .then(liveOrders => {
         console.log('======= ACCOUNT LIVEORDERS =======');
         console.log(liveOrders)
     })
-    .then(() => TastyWorks.history(process.env.TASTY_ACCOUNT_ID, '01/01/2019', '01/05/2019'))
+    .then(() => TastyWorks.history(TASTY_ACCOUNT_ID, '01/01/2019', '01/05/2019'))
+    .catch(err => console.log(err.status))
     .then(history => {
         console.log('======= ACCOUNT HISTORY =======');
         console.log(history)
